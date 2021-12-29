8889603899c09464807b2164f118d853aad21cc2
mkyu0917 <mkyu0917@gmail.com>
2021-12-29 16:36:05 +0900

    회원관리예제 (회원조회)

--- src/main/java/com/hello/hellospring/controller/HomeController.java
+++ src/main/java/com/hello/hellospring/controller/HomeController.java
@@ -5,9 +5,12 @@ import com.hello.hellospring.domain.MemberForm;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -47,4 +50,10 @@ public class HomeController {
         return "redirect:/";
     } ;
 
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
